1bd8f3b87b22df599bd48974a67955ee77cf7252
huangl <136748005@qq.com>
2020-04-20 11:01:51 +0800

    test

---
+++ src/main/java/com/timegroup/financecloud/FinanceCloudApplication.java
@@ -0,0 +1,13 @@
+package com.timegroup.financecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/timegroup/financecloud/FinanceCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timegroup.financecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
